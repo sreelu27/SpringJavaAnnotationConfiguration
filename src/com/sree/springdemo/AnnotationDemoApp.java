0b8f0bfcb096c14fc1e17e325ffac0ef2f14f9fb
sgeetha2 <sgeetha2@its.jnj.com>
2020-03-23 17:12:25 +0000

    initial commit

---
+++ src/com/sree/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,20 @@
+package com.sree.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new
+				 ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach = context.getBean("thatSillyCoach",Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		context.close();
+
+	}
+
+}
---
+++ src/com/sree/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.sree.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+
+}
